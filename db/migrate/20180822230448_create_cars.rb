554f9ac86577e0c3409d4fd6ad3c5029723e7056
Pablo Fernandez <pfernandez1980@gmail.com>
2018-08-23 15:45:00 -0700

    updates for heroku deployemnt2

--- db/migrate/20180822230448_create_cars.rb
+++ db/migrate/20180822230448_create_cars.rb
@@ -2,7 +2,7 @@ class CreateCars < ActiveRecord::Migration[5.2]
   def change
     create_table :cars do |t|
       t.string :make
-      t.sting :model
+      t.string :model
       t.integer :year
 
       t.timestamps
